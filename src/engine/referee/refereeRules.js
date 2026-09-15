6af115bcbb0af43d0132e49f069838e948b7ef79
Leg3ndS <59032491+Leg3ndS@users.noreply.github.com>
2026-09-15 15:19:23 +0200

    Add files via upload

--- src/engine/referee/refereeRules.js
+++ src/engine/referee/refereeRules.js
@@ -1,22 +1,3 @@
-// Core football restart/ruling helpers used by the referee.
-
-export const RESTARTS = Object.freeze({
-  KICKOFF: "kickoff",
-  GOAL_KICK: "goal_kick",
-  CORNER: "corner",
-  THROW_IN: "throw_in",
-  FREE_KICK: "free_kick",
-  PENALTY: "penalty",
-  DROPPED_BALL: "dropped_ball",
-});
-
-export const FOULS = Object.freeze({
-  NONE: "none",
-  CARELESS: "careless",
-  RECKLESS: "reckless",
-  EXCESSIVE_FORCE: "excessive_force",
-});
-
-export function isDirectFreeKickFoul(type) {
-  return type !== FOULS.NONE;
-}
+export const RESTARTS=Object.freeze({KICKOFF:'kickoff',GOAL_KICK:'goal_kick',CORNER:'corner',THROW_IN:'throw_in',FREE_KICK:'free_kick',PENALTY:'penalty',DROPPED_BALL:'dropped_ball'});
+export const FOULS=Object.freeze({NONE:'none',CARELESS:'careless',RECKLESS:'reckless',EXCESSIVE_FORCE:'excessive_force'});
+export const isDirectFreeKickFoul=(type)=>type!==FOULS.NONE;
